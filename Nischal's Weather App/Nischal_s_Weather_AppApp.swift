e43c0cd0fa811b95e2830df70dff79ea423e47a7
Nischal Niroula <officialnischalniroula@gmail.com>
2023-07-19 22:52:04 +1000

    Initial Commit

---
+++ Nischal's Weather App/Nischal_s_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nischal_s_Weather_AppApp.swift
+//  Nischal's Weather App
+//
+//  Created by Nischal Niroula on 19/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct Nischal_s_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
